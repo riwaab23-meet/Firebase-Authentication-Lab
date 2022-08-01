bdde7fb470169e5b99e82727d874d0bb4b6e3c8d
riwaab23-meetmeow <109904805+riwaab23-meetmeow@users.noreply.github.com>
2022-08-01 18:59:07 +0300

    meow
    there is some bugs in it but i did not know how to fix it
    sorry i will try again tomorrow or at night
    
    sincerely,
                    meow(_riwa)

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -16,6 +16,7 @@ config = {
 
 firebase=pyrebase.initialize_app(config)
 auth=firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -23,44 +24,77 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-   error = ""
-   if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('home'))
-       except:
-           error = "Authentication failed"
-           return render_template("signup.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		bio = request.form['bio']
+		full_name = request.form['full_name']
+		username = request.form['username']
 
-   else:
-       print(f"Didn't get to post. got to: {request.method}")
-       return render_template("signup.html")
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			user = {"name": full_name, "username": username , "bio" : bio,"email":email,"password":password}
+			db.child("Users").child(login_session['user']['localId']).set(user)
+
+			return redirect(url_for('signin'))
+		except:
+			error = "Authentication failed"
+			return render_template("signup.html")
+
+	else:
+		print("Didn't get to post.")
+		return render_template("signup.html")
 
 
-@app.route('/signin', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def signin():
-   error = ""
-   if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-               login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-               return redirect(url_for('home'))
-       except:
-           error = "Authentication failed"
-
-   else:
-   	print(f"Didn't get to post. got to: {request.method}")
-   	return render_template("signin.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+			return render_template("signin.html")
+	else:
+		print(f"Didn't get to post. got to: {request.method}")
+		return render_template("add_tweet.html")
 
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+	error = ""
+	if request.method == 'POST':
+		text = request.form['text']
+		title = request.form['title']
+
+		try:
+			login_session['tweet'] = auth.sign_in_with_email_and_password(email, password)
+			user = {"text": text, "title": title , "uid" : login_session['user']['localId']}
+			db.child("tweets").push(user)
+			return redirect(url_for('home'))
+		except:
+			error = "Authentication failed"
+			return render_template("add_tweet.html")
+
+	else:
+		print("Didn't get to post. got to: {request.method}")
+		return render_template("add_tweet.html")
+
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def tweets():
+	tweets = db.child("Tweets").get().val()
+	return render_template("signin.html")
+
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
